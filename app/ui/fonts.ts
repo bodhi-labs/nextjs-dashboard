b2c0bf150a0d33bdbef5136125f1dd4be6319fd1
bodhi-labs <128878476+bodhi-labs@users.noreply.github.com>
2024-03-22 13:39:04 +0800

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-export const lusitana = Lusitana({
+export const lusitana = lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
   });
